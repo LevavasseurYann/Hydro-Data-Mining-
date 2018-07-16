5ba6c05b741bbf9535448f71cb6132552a966239
Yann <levavasseuryann@gmail.com>
2018-07-16 13:37:16 +1100

    up conf

--- docs/conf.py
+++ docs/conf.py
@@ -14,10 +14,24 @@
 #
 import os
 import sys
+import mock
 
 sys.path.insert(0, os.path.abspath('../'))
 
+MOCK_MODULES = [
+        'numpy',
+        'pandas',
+        "sklearn",
+        "sklearn.preprocessing",
+        "sklearn.decomposition",
+        "sklearn.cluster",
+        "sklearn.metrics",
+        "sklearn.linear_model",
+        "matplotlib",
+        "matplotlib.pyplot"]
 
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 # -- Project information -----------------------------------------------------
 
 project = 'Internship_Levavasseur_Yann'
