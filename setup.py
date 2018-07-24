5244c903205e07710bc3210c545fff4c2ca43024
Yann <levavasseuryann@gmail.com>
2018-07-24 13:08:47 +1100

    clean req

--- setup.py
+++ setup.py
@@ -15,18 +15,4 @@ maintainer = "LEVAVASSEUR Yann",
 maintainer_email = "levavasseuryann@gmail.com",
 keywords = "python",
 url = "https://github.com/LevavasseurYann/workspace_internship",
-download_url = 'https://github.com/LevavasseurYann/workspace_internship.git',
-test_suite='nose.collector',
-tests_require=['nose'],
-classifiers = [
-	'Intended Audience :: Science/Research',
-	'Intended Audience :: Developers',
-	'Programming Language :: Python',
-	'Topic :: Software Development',
-	'Topic :: Scientific/Engineering',
-	'Operating System :: Microsoft :: Windows',
-	'Operating System :: Unix',
-	'Operating System :: MacOS',
-	'Programming Language :: Python :: 3.5',
-	'Programming Language :: Python :: 3.6']
 )
