537cca69929c622c56525465de79cea92f57c187
Yann <levavasseuryann@gmail.com>
2018-07-24 14:09:32 +1100

    clean req

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 import re
 import os
-cmd = 'pip install --upgrade pip'
+cmd = 'pip3 install --upgrade pip'
 os.system(cmd)
 
 # ------------------
 
 setup(
 name = "workspace_internship",
-version = "1.0.0",
+version = "1.0",
 description='M2 internship project',
 packages=find_packages(),
 author = "LEVAVASSEUR Yann",
