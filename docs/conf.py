c6ecfd070fa76fb13ee18346208035e16564378f
Yann <levavasseuryann@gmail.com>
2018-07-30 12:21:00 +1100

    Mock install probleme sur l'env RTD

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['tslearn', 'prefixspan', 'numpy', 'pandas', 'collections', 'pickle', 'scipy', 'mplot3d']
+MOCK_MODULES = ['tslearn', 'prefixspan', 'numpy', 'pandas', 'pickle', 'scipy', 'mplot3d']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 #sys.path.insert(0, os.path.abspath('../'))
